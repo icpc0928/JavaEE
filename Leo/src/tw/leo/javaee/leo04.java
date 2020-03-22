39fdc60b6e35ea8fb34d8d92b7d26089a2fd2fb4
Leo <icpc0928@gmail.com>
2020-03-22 14:14:38 +0800

    asdf

--- Leo/src/tw/leo/javaee/leo04.java
+++ Leo/src/tw/leo/javaee/leo04.java
@@ -2,6 +2,8 @@ package tw.leo.javaee;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,13 +19,33 @@ public class leo04 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		
-			BufferedReader reader = request.getReader();
-			String line;
-			while((line = reader.readLine()) != null) {
-				System.out.println(line);
+		
+			request.setCharacterEncoding("UTF-8");
+		
+//			BufferedReader reader = request.getReader();
+//			String line;
+//			while((line = reader.readLine()) != null) {
+//				System.out.println(line);
+//			}
+//			reader.close();
+			
+			InputStream in = request.getInputStream();
+			byte[] buf = new byte[4096]; int len;
+			while((len = in.read(buf))!= -1) {
+				System.out.println(new String(buf,0,len));
 			}
-			reader.close();
+			in.close();
+			
+			System.out.println("-------");
+			String OK = request.getParameter("OK");
+			System.out.println(OK);
+			
 	
-
 }
+	
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
 }
